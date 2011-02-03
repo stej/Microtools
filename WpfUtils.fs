d17772d6556f63dc91d7a7d7ec07d75c498c1bb8
stej <stej@leporelo.eu>
2011-02-03 21:23:47 +0100

    Displayed status is parsed and urls made to hyperlinks.
    It is possible to choose either big or small controls (text size and picture) in config file.

--- WpfUtils.fs
+++ WpfUtils.fs
@@ -12,48 +12,94 @@ open System.Windows.Media
 open System.Diagnostics
 open Status
 
-let createLittlePicture (status:status) = 
+let private regexUrl = new System.Text.RegularExpressions.Regex("(https?:(?://|\\\\)+[\w\d:#@%/;$()~_?+\-=\\\.&*]*[\w\d:#@%/;$()~_+\-=\\&*])")
+
+let (fontSize, pictureSize) = 
+    match System.Configuration.ConfigurationManager.AppSettings.["size"].ToString() with
+    | "big" -> (14., 48.)
+    | _ -> (12., 30.)
+
+let private createPicture size margin (status:status) = 
     let source = new Imaging.BitmapImage()
     let imagePath = ImagesSource.getImagePath status
     source.BeginInit();
     source.UriSource <- new Uri(imagePath, System.UriKind.Relative);
-    source.DecodePixelWidth <- 30
+    source.DecodePixelWidth <- int pictureSize
     source.EndInit()
     new Image(Source = source,
               Name = "image",
-              Margin = new Thickness(2.),
-              Width = 30.,
-              Height = 30.,
+              Margin = margin,
+              Width = pictureSize,
+              Height = pictureSize,
               ToolTip = new ToolTip(Content = (sprintf "%s %A\n%s" status.UserName status.Date status.Text)),
               VerticalAlignment = VerticalAlignment.Top)
-    //Background = Brushes.Gray
+              //Stretch <- Media.Stretch.Uniform
+
+let private BrowseHlClick (e:Navigation.RequestNavigateEventArgs) =
+    Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri)) |> ignore
+    e.Handled <- true
+
+let private textToTextblock (text:string) = 
+    let parts = regexUrl.Split(System.Web.HttpUtility.HtmlDecode(text))
+
+    let ret = new TextBlock(TextWrapping = TextWrapping.Wrap,
+                            Padding = new Thickness(0.),
+                            Margin = new Thickness(5., 0., 0., 5.),
+                            FontSize = fontSize)
+    parts 
+    |> Seq.iter (fun part ->
+        if regexUrl.IsMatch(part) then
+            let hl = new Hyperlink(new Run(part),
+                                   NavigateUri = new Uri(part))
+            hl.RequestNavigate.Add(BrowseHlClick)
+            ret.Inlines.Add(hl)
+        else
+            ret.Inlines.Add(new Run(part))
+    )
+    ret
+
+let createLittlePicture status = 
+    createPicture 30. (new Thickness(2.)) status 
               
 let createDetail (status:status) =
     let row = new StackPanel(HorizontalAlignment = HorizontalAlignment.Left,
                              VerticalAlignment = VerticalAlignment.Top,
                              Orientation = Orientation.Horizontal)
-    let textInfo = new StackPanel(HorizontalAlignment = HorizontalAlignment.Left,
-                                  VerticalAlignment = VerticalAlignment.Top,
-                                  Orientation = Orientation.Vertical,
-                                  Width = 500.)
-    let img = 
-        let i = createLittlePicture status
-        //i.Stretch <- Media.Stretch.Uniform
-        i.Margin <- new Thickness(5., 0., 0., 5.)
-        i
-    let nameAndId = 
-        let hl = new Hyperlink(new Run(sprintf "%s - %d - %s" status.UserName status.StatusId (status.Date.ToString("yyyy-MM-dd HH:mm:ss"))),
-                                       NavigateUri = new Uri(sprintf "http://twitter.com/#!/%s/status/%d" status.UserName status.StatusId))
-        hl.RequestNavigate.Add(fun e -> Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri)) |> ignore; e.Handled <- true )
-        new Label(Content = hl,
-                  Padding = new Thickness(0.),
-                  Margin = new Thickness(5., 0., 0., 2.))
-    let text = new TextBlock(Text = status.Text, 
-                             TextWrapping = TextWrapping.Wrap,
-                             Padding = new Thickness(0.),
-                             Margin = new Thickness(5., 0., 0., 5.))
-    textInfo.Children.Add(nameAndId) |> ignore
-    textInfo.Children.Add(text) |> ignore
+    let meta =
+        let m = new TextBlock(TextWrapping = TextWrapping.Wrap,
+                              Padding = new Thickness(0.),
+                              Margin = new Thickness(5., 0., 0., 5.))
+        let hl = 
+          let l = new Hyperlink(new Run(sprintf "%d" (status.StatusId)),
+                                NavigateUri = new Uri(sprintf "http://twitter.com/#!/%s/status/%d" status.UserName status.StatusId))
+          l.RequestNavigate.Add(BrowseHlClick)
+          l
+        [new Run(status.UserName) :> Inline
+         new Run(" | ")           :> Inline
+         hl                       :> Inline
+         new Run(" | ")           :> Inline
+         new Run(sprintf "%s" (status.Date.ToString("yyyy-MM-dd HH:mm:ss"))) :> Inline]
+        |> List.iter m.Inlines.Add
+        m
+
+    let imgContent = createPicture pictureSize (new Thickness(5., 0., 0., 5.)) status
+    let img = new Border(BorderBrush = Brushes.LightGray,
+                     BorderThickness = new Thickness(0., 0., 0., 0.),
+                     CornerRadius = new CornerRadius(2.),
+                     Child = imgContent)
+
+    let textInfo =
+        let s = new StackPanel(HorizontalAlignment = HorizontalAlignment.Left,
+                               VerticalAlignment = VerticalAlignment.Top,
+                               Orientation = Orientation.Vertical,
+                               Width = 500.,
+                               Margin = new Thickness(0.))
+        s.Children.Add(meta) |> ignore
+        s.Children.Add((textToTextblock status.Text)) |> ignore
+        new Border(BorderBrush = Brushes.LightGray,
+                   BorderThickness = new Thickness(0., 0., 0., 1.),
+                   Child = s)
+    
     row.Children.Add(img) |> ignore
     row.Children.Add(textInfo) |> ignore
     (row, img)
@@ -107,7 +153,7 @@ let updateConversation (showAsNew:status -> bool) (controls:conversationControls
 
     let rec addTweets depth currentStatus (showAsNew:status -> bool) =
         let detail, img = createDetail currentStatus
-        img.Margin <- new Thickness(depth * 32., 0., 0., 5.)
+        img.Margin <- new Thickness(depth * (pictureSize+2.), 0., 0., 5.)
         if (showAsNew currentStatus) then detail.Background <- Brushes.Yellow
         controls.Statuses.Children.Add(detail) |> ignore
         currentStatus.Children |> Seq.iter (fun s -> addTweets (depth+1.) s showAsNew)
@@ -116,19 +162,10 @@ let updateConversation (showAsNew:status -> bool) (controls:conversationControls
 /// partial, set just the function
 let setNewConversation = updateConversation (fun _ -> false)
     
-//let createAppState() =
-//   let sp = new StackPanel(Orientation = Orientation.Horizontal)
-//    sp.Children.Add(new Label(Content = "Twitter limit: ")
-//    sp.Children.Add(
-
 let createXamlWindow (file : string) = 
   use xmlReader = XmlReader.Create(file)
   System.Windows.Markup.XamlReader.Load(xmlReader) :?> Window
   
-//let dispatchMessage<'a> (dispatcherOwner:DispatcherObject) fce arguments = 
-//    dispatcherOwner.Dispatcher.Invoke(DispatcherPriority.Normal, 
-//                                      System.Action<'a>(fce), 
-//                                      arguments) |> ignore
 let dispatchMessage<'a> (dispatcherOwner:DispatcherObject) fce = 
     dispatcherOwner.Dispatcher.Invoke(DispatcherPriority.Normal, 
                                       System.Action<'a>(fce), 
