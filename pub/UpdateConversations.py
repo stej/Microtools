e4ea3373f249be6701d2115f0a8a9563a40e8888
stej <stej@leporelo.eu>
2011-11-25 18:26:57 +0100

    update of UpdateConversations.py to download personal statuses as well

--- pub/UpdateConversations.py
+++ pub/UpdateConversations.py
@@ -4,6 +4,7 @@ from StatusesReplies import *
 statuses = db.GetRootStatusesHavingReplies(400)
 res = []
 i = 0
+personalStatusesDownloaded = False
 while i < statuses.Length:
   #if limits.GetLimits().SearchLimit != None and limits.GetLimits().SearchLimit.Value > System.DateTime.Now:
   if System.Console.KeyAvailable:
@@ -11,17 +12,25 @@ while i < statuses.Length:
     if keyinfo.Key == System.ConsoleKey.Escape:
       print "Pressed Esc -> breaking"
       break
+      
   if not limits.IsSafeToQueryTwitter():
+    # lets try to download perosnal statuses.. assuming that 2 requests are ok :)
+    if not personalStatusesDownloaded:
+      statuses = h.DownloadAndSavePersonalStatuses()
+      print 'Personal statuses count: {0}'.format(statuses.Length)
+      for status in statuses:
+        print '{0}'.format(status.ToString())
+      personalStatusesDownloaded = True
+
     print '[{0}] {1} - {2}'.format(i, limits.GetLimitsString(), System.DateTime.Now)
-    #System.Threading.Thread.Sleep(5000)
-    System.Threading.Thread.CurrentThread.Join(1000*60*5)
+    System.Threading.Thread.CurrentThread.Join(1000*60*10)
     continue
   print "\n----- [{0}] - {1}\n".format(i, limits.GetLimitsString())
   status = statuses[i]
   h.loadTree(status)
   print status.ToString()
-  #h.show(status)
+  
   withrepl = findReplies(status)
   res.append(withrepl)
   i = i+1
-#h.show(res)
+  personalStatusesDownloaded = False
